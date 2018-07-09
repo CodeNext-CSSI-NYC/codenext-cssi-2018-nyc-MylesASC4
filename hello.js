7683b86ea6f7b5bfc83b9b721b89280913b1c33b
Myles N <myles.nutifafa@gmail.com>
2018-07-09 15:39:13 -0400

    Hello world demo

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
 // Author: FirstName LastName
+console.log("Jello, World");console.log("It's a beautiful day for programming, isn't it?");
+console.log("Hey! What's 2 + 2 ?");
+console.log("It's " + 2 + 2 + " Minus 1 thats " + 3 + ", quickmaths");
+console.log("No, it's " + (2 + 2) + ", silly goose!");
+// console.log("This isn't going to get printed, is it?");
