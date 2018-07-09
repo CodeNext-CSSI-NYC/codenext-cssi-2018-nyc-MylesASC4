a30d30fe136743c3101369a0f9dea42d856fb7c1
Myles N <myles.nutifafa@gmail.com>
2018-07-09 16:36:43 -0400

    Answered all the questions

--- math-practice.js
+++ math-practice.js
@@ -1,6 +1,6 @@
 // Under each question, write the code that solves it, and then write your answer in a comment
 // For example, if the question is: What's the sum of 79 and 2018 you would write:
-console.log(79 + 2018);
+// console.log(79 + 2018);
 // 2097
 
 // Some of the questions will use these variables
@@ -8,16 +8,26 @@ const p = 12;
 const q = 60;
 const r = 25;
 
-// 1. Is 91 divisible by 13? (The answer should be either true or false)
+console.log("1. Is 91 divisible by 13? (The answer should be either true or false)")
+if(91%13 == 0){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+console.log(" 2. What is the result when the q divided by r is rounded to the nearest integer?")
+console.log( Math.round(q/r))
 
-// 2. What is the result when the q divided by r is rounded to the nearest integer?
+console.log("3. Is the product of p and r greater than q squared? (The answer should be either true or false)")
+console.log( (p*q) > (q**2) )
 
-// 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
+console.log("4. What is the remainder when the sum of q and r is divided by p?")
+console.log( (q+r)%p)
 
-// 4. What is the remainder when the sum of q and r is divided by p?
-
-// 5. 27 squared is equal to what number raised to the third power?
-
+console.log("5. 27 squared is equal to what number raised to the third power?")
+console.log( (27**2) + " = 9**3 = " + Math.cbrt(27**2)**3)
 // 6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)
-
+console.log("6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)")
+console.log( Math.abs(p - q))
 // 7. Find another math operation we haven't talked about yet on the MDN (Mozilla Developer Network) and try it out below.
+console.log("Random #. Math.random = " + Math.random(45)*Math.floor(45))
