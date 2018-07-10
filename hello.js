dd3b260db182eb291eb2188d1dfaf1b3ea858fb5
Myles N <myles.nutifafa@gmail.com>
2018-07-10 10:28:39 -0400

    Init'd npm, updated Hello

--- hello.js
+++ hello.js
@@ -1,6 +1,10 @@
-// Author: FirstName LastName
-console.log("Jello, World");console.log("It's a beautiful day for programming, isn't it?");
-console.log("Hey! What's 2 + 2 ?");
-console.log("It's " + 2 + 2 + " Minus 1 thats " + 3 + ", quickmaths");
-console.log("No, it's " + (2 + 2) + ", silly goose!");
-// console.log("This isn't going to get printed, is it?");
+var readline = require("readline-sync")
+
+let name = readline.question("Whats your name homie? :")
+console.log("Whats poppin big Homie: " + name)
+// Author: Myles  LastName
+// console.log("Jello, World");console.log("It's a beautiful day for programming, isn't it?");
+// console.log("Hey! What's 2 + 2 ?");
+// console.log("It's " + 2 + 2 + " Minus 1 thats " + 3 + ", quickmaths");
+// console.log("No, it's " + (2 + 2) + ", silly goose!");
+// // console.log("This isn't going to get printed, is it?");
