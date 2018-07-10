3ba545b06a0352010b5b1a48058a2986a4bc0952
Myles N <myles.nutifafa@gmail.com>
2018-07-10 12:38:10 -0400

    Change machine worked!

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,25 @@
-// Author: FirstName LastName
+// Author: Myles Nutifafa
+var input = require("readline-sync")
+let coins = parseInt(input.question(" HELLO I AM CHANge MACHINE: INPUT CHANGE VALUE: "))
+
+const Nickel = 5
+const Quarter = 25
+const Penny = 1
+const Dime = 10
+
+const qNum = coins/Quarter
+const dNum = (coins%Quarter)/Dime
+const nNum = (coins%Dime)/Nickel
+const pNum = (coins%Dime)/Penny
+
+const qVal = Quarter*qNum
+const nVal = Nickel*nNum
+const pVal = Penny*pNum
+const dVal = Dime*dNum
+
+coins = coins - qVal - dVal - pVal - nVal   
+console.log( "Quarters: " + Math.floor( qNum));
+console.log( "Dimes: " + Math.floor(dNum))
+console.log( "Nickels: " + Math.floor(nNum))
+console.log( "Pennies: " + Math.floor(pNum))
+
