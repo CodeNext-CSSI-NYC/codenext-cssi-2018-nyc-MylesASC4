4f8b2f29ae48f8bc13271fe323bd3d4f6cd27dc3
Myles N <myles.nutifafa@gmail.com>
2018-07-17 17:00:17 -0400

    Lots of practices, Objs and more Arrays (Mostly Objects)

---
+++ GitClan.js
@@ -0,0 +1,9 @@
+// Git Clan
+
+// 1: Code a function that takes a random letter from a string and prints a random letter
+// Reward: House
+
+
+
+//2: Code a program that Spells the Weekdays Numerically (eg: Monday = 13, 15,14,4,1,25)
+// Reward: Town Hall
